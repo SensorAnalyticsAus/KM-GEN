fcecbe6e290fa7da450a08e0771de2bded675c00
SensorAnalyticsAus <114129496+SensorAnalyticsAus@users.noreply.github.com>
2024-09-19 22:29:12 +1000

    update

--- config.py
+++ config.py
@@ -5,18 +5,18 @@ ImgPath='../../../FILES/yt/images-polar' # images path
 cSz=5                                       # n imagefiles/workpacket
 wdir='./tmp-pkl'                            # path to working tmp dir
 imght=30   # adjust full image height to imght pixels keep same A/R
-nfts=100   # features required 
+nfts=10   # features required 
 ktol=1e-6  # default 1e-4
 kiter=1000 # set max_iter value, default=300
 kmethod='k-means++' # random or k-means++
-ninit=10   # a numeric value e.g. 10 or 'auto', later is non-deterministic
+ninit=100  # a numeric value e.g. 10 or 'auto', later is non-deterministic
 loadfit=0  # '0' use labels from training '1' recompute labels with fit_predict
 saverej='no' # 'yes' or 'no' save or don't save rejected image file names
 rseed=20 # any number for reproduceable indices for imgdist or 'off' to disable
 deBug=0    # '0' quiet mode '1' feature forming messages '2' more info
 imgfull=0  # '0' disables full image | '1' uses full image overrides imgdist
 img_bw=0   # '0' use default grayscale | '1' change to black and white
-imgdist=1  # '0' use keypoint features | '1' use descriptors' hamming dists
+imgdist=2  # '0' use keypoint features | '1' use descriptors' hamming dists
            # '2' use Hu's invariants
 #
 # Usage:
