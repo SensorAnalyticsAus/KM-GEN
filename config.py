4a9db2aab2978610318e2bd75f9bfd1ece4615e7
Asad I. Khan <sensoranalytics101@gmail.com>
2025-12-31 00:49:34 +1100

    reset

--- config.py
+++ config.py
@@ -1,8 +1,7 @@
 # KM-GEN CONFIG
 #
 c_old=0   # '0' compute new training data '1' re-use training data from lastrun
-#ImgPath='./flowers/data' # images path 
-ImgPath='/mnt/WD6TB_PI4C/nn/night/c500' # images path 
+ImgPath='./flowers/data' # images path 
 cSz=5                                       # n imagefiles/workpacket
 wdir='./tmp-pkl'                            # path to working tmp dir
 nfts=10    # features required 
